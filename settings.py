bf7adf3ae3e7a0bb96b323759de01739f57b6088
Vladislav Prozorov <prozorov93@gmail.com>
2023-04-05 16:38:36 +0300

    update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 import json
+import os.path
 
 
 def get_settings():
-    with open('database/settings.json', 'rt', encoding='utf-8') as file:
+    with open(os.path.join(os.getcwd(), 'database', 'settings.json'), 'rt', encoding='utf-8') as file:
         settings = json.loads(file.read())
 
     return settings
